a8319b1b0f5323ca225e6cd55219d59cd174f12a
Jacob Tovsen <jacobtovsen@gmail.com>
2018-05-31 13:52:16 -0500

    working through postman

---
+++ server/routers/movie.router.js
@@ -0,0 +1,37 @@
+const express = require('express');
+const router = express.Router();
+movieId = 2;
+const movieArray = [
+    {
+        title: 'Star Wars',
+        id: 1
+    }
+];
+
+router.get('/', (req, res)=>{
+    console.log(`In /movie GET...`);
+    res.send(movieArray);
+})
+
+router.post('/', (req, res) => {
+    let movie = req.body;
+    console.log(`in /movie POST with data ${movie}`);
+    movie.id = movieId++; // pretend we are a database
+    movieArray.push(movie);
+    res.sendStatus(201);
+})
+
+router.delete('/', (req, res)=>{
+    let id = req.query.id;
+    console.log(`In /movie DELETE...`);
+    for (let i=0; i<movieArray.length; index ){
+        let movie = movieArray[i];
+        if(id === movie.id){
+            movieArray.splice(i,1);
+        }
+    }
+    // vm.movies.splice( index, 1 );
+    res.sendStatus(200);
+})
+
+module.exports = router;
--- server/server.js
+++ server/server.js
@@ -1,10 +1,20 @@
 // includes
 let express = require( 'express' );
-let app = express()
+let bodyParser = require( 'body-parser' );
+let movieRouter = require( './routers/movie.router' );
+let app = express();
+
+
 // globals
 let port = 5000 || process.env.PORT;
+
+
 //uses
 app.use( express.static( 'server/public' ) );
+app.use( bodyParser.urlencoded( { extended: true } ) );
+app.use( bodyParser.json() );
+app.use( '/movie', movieRouter );
+
 //server up
 app.listen( port, ()=>{
     console.log( 'server up on:', port );
